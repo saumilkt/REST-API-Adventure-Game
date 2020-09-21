9be8769394754f767c9ba28394eaa14eb9b270c0
Saumil Thakore <saumilkt@gmail.com>
2020-09-20 19:56:31 -0700

    added pong response to ping() function

--- src/main/java/student/server/AdventureResource.java
+++ src/main/java/student/server/AdventureResource.java
@@ -9,7 +9,7 @@ public class AdventureResource {
     /**
      * The single static adventure service instance used for this API.
      */
-    private static AdventureService service; // = new YourAdventureServiceHere();
+    private static AdventureService service = new AdventureServiceImpl();
 
     /**
      * The API endpoint to test connectivity.
@@ -19,7 +19,7 @@ public class AdventureResource {
     @Path("ping")
     public String ping() {
         // TODO: This method should return `pong`.
-        return "";
+        return "pong";
     }
 
     /**
